291ba746ccf14d9beb560e48a20691433ccb1c34
Adam Dias <adam.dias@outlook.com>
2022-03-14 04:12:15 -0300

    test: add http-client package with axios

---
+++ src/packages/http-client/app/axios/axios-http-client-mock.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { mockHttpResponse } from "../../core/http-client.mock";
+
+const mockAxios = (): jest.Mocked<typeof axios> => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  mockedAxios.request.mockClear().mockResolvedValue(mockHttpResponse());
+  return mockedAxios;
+};
+
+export default mockAxios;
