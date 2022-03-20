03757c6f43bfba972e543e1cb2d87e8e94230fe7
Adam Dias <adam.dias@outlook.com>
2022-03-20 20:41:01 -0300

    feat: add CarIcon

---
+++ src/packages/ui-kit/icon/car/car.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import CarIcon from "./car";
+import { IconSvgProps } from "../icon.types";
+
+export default {
+  title: "Icons/Car",
+  component: CarIcon,
+} as ComponentMeta<typeof CarIcon>;
+
+const CarIconTemplate: ComponentStory<typeof CarIcon> = ({
+  width,
+  height,
+  color,
+}: IconSvgProps) => <CarIcon width={width} height={height} color={color} />;
+
+export const Car = CarIconTemplate.bind({});
+
+Car.args = {
+  width: 35,
+  height: 35,
+  color: "#c41333",
+};
---
+++ src/packages/ui-kit/icon/icon.types.ts
@@ -0,0 +1,9 @@
+export type IconSvgProps = {
+  width: number;
+  height: number;
+  color: string;
+};
+
+export type IconProps = {
+  name: "car";
+} & IconSvgProps;
