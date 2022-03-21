62f05dbd27539063f5582519aa217eb84019eb6d
Adam Dias <adam.dias@outlook.com>
2022-03-20 23:25:07 -0300

    fix: add viewBox in LocationPinIcon

--- src/packages/ui-kit/icon/location-pin/location-pin.tsx
+++ src/packages/ui-kit/icon/location-pin/location-pin.tsx
@@ -8,8 +8,7 @@ const LocationPinIcon = ({ width, height, color }: IconProps) => {
         width="100%"
         height="100%"
         xmlns="http://www.w3.org/2000/svg"
-        fillRule="evenodd"
-        clipRule="evenodd"
+        viewBox="0 0 24 24"
       >
         <path
           fill={color}
