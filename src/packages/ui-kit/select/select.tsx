24af795ea1bc9315d8194f6d65c9bf01069a164d
Adam Dias <adam.dias@outlook.com>
2022-03-29 13:40:02 -0300

    feat: create disabled and loading props in Select component

--- src/packages/ui-kit/select/select.tsx
+++ src/packages/ui-kit/select/select.tsx
@@ -1,42 +1,53 @@
 import React from "react";
 import { SelectProps } from "./select.types";
-import { ArrowDownIcon } from "../icon/arrow-down";
 
 import "../input/input.styles.scss";
 import "./select.styles.scss";
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
-  ({ label, name, error, options, state, setState }, ref) => {
+  (
+    { label, name, error, loading, disabled = false, options, state, setState },
+    ref
+  ) => {
     const errorCssClass = error ? " input__box--error" : "";
+    const disabledCssClass = disabled ? " select__label--disabled" : "";
 
     return (
-      <label className="select__label input__label" htmlFor={name}>
+      <label
+        className={`input__label select__label${disabledCssClass}`}
+        htmlFor={name}
+      >
         <div className={`input__box${errorCssClass}`}>
           <div className="input__box--left-side">
             <span>{label}</span>
           </div>
 
-          <select
-            ref={ref}
-            id={name}
-            name={name}
-            className="select__field input__field"
-            onChange={e => {
-              if (state && setState) {
-                setState({ ...state, [e.target.name]: e.target.value });
-              }
-            }}
-          >
-            {options.map(option => (
-              <option value={option.value} selected={option.selected}>
-                {option.label}
-              </option>
-            ))}
-          </select>
+          {loading && <span>carregando...</span>}
 
-          <div className="select__icon">
-            <ArrowDownIcon width={5} height={5} color="#000" />
-          </div>
+          {!loading && (
+            <select
+              ref={ref}
+              id={name}
+              name={name}
+              className="select__field input__field"
+              onChange={e => {
+                if (state && setState) {
+                  setState({ ...state, [e.target.name]: e.target.value });
+                }
+              }}
+              disabled={disabled}
+            >
+              {options.map(option => (
+                <option
+                  key={`${option.label}${option.value}`}
+                  value={option.value}
+                  selected={option.selected}
+                >
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          )}
         </div>
 
         {error && <span className="input__error text--lg">{error}</span>}
--- src/packages/ui-kit/select/select.types.ts
+++ src/packages/ui-kit/select/select.types.ts
@@ -2,7 +2,13 @@ export type SelectProps = {
   label: string;
   name: string;
   error?: string;
-  options: { label: string; value: string | number; selected?: boolean }[];
+  disabled?: boolean;
+  loading?: boolean;
+  options: {
+    label: string;
+    value: string | number;
+    selected?: boolean;
+  }[];
   state?: any;
   setState?: any;
 };
