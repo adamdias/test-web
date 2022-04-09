fe030aa59ea0d7ae88b472d9bf6a4c19ab1ccde3
Adam Dias <adam.dias@outlook.com>
2022-04-08 21:47:15 -0300

    feat: add cssType link in Button component

--- src/packages/ui-kit/button/button.tsx
+++ src/packages/ui-kit/button/button.tsx
@@ -7,7 +7,7 @@ const Button = ({ title, type = "button", cssType, onClick }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`button button-${cssType}`}
+      className={`button button--${cssType}`}
       onClick={onClick && onClick}
     >
       <span className="font--bold">{title}</span>
--- src/packages/ui-kit/button/button.types.ts
+++ src/packages/ui-kit/button/button.types.ts
@@ -3,6 +3,6 @@ import React, { ButtonHTMLAttributes } from "react";
 export type ButtonProps = {
   title: string;
   type: ButtonHTMLAttributes<HTMLButtonElement>["type"];
-  cssType: "primary" | "outline";
+  cssType: "primary" | "outline" | "link";
   onClick?: (event: React.MouseEvent<HTMLElement>) => void;
 };
